3d84d2f2a96dae8e3ada58791e7e8d9563ebd5a3
iratsaruk13 <115734138+iratsaruk13@users.noreply.github.com>
2023-07-07 23:53:18 +0300

    completed all tasks

---
+++ README.md
@@ -0,0 +1,2 @@
+Link to screenshots:
+https://monosnap.com/list/64a87924e1b529d7b0cd805c
--- contacts.js
+++ contacts.js
@@ -1,50 +1,84 @@
+// імпорт пакета fs у версії з промісами для зручної роботи з async await
 const fs = require("fs/promises");
+// імпорт path для правильного створення шляхів
 const path = require("path");
+const { nanoid } = require("nanoid");
 
-
+// створення абсолютного шляху; join нормалізує шлях (правильно розставляє / для різних ОС)
 const contactsPath = path.join(__dirname, "./db/contacts.json");
 
 const listContacts = async () => {
-const data = await fs.readFile(contactsPath);
-return JSON.parse(data);
-}
+  // функція читає json-файл з контактами
+  const data = await fs.readFile(contactsPath);
+  // проганяємо через JSON.parse для отримання масиву об'єктів
+  return JSON.parse(data);
+};
 
 const getContactById = async (id) => {
-const contactsId = String(id);
-const contacts = await listContacts();
-const result = contacts.find(el => el.id === contactsId);
-return result || null;
-}
-
-const removeContact = async (id) => {
-    const contactId = String(id);
-    const contacts = await listContacts();
-    const index = contacts.findIndex(el => el.id === contactId);
-    if(index === -1) {
-        return null;
-    }
-    const [result] = contacts.splice(index, 1);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return result;
-
-}
+  const contactId = String(id);
+  // отримуємо всі контакти
+  const contacts = await listContacts();
+  // шукаємо потрібний контакт
+  const result = contacts.find((el) => el.id === contactId);
+  // якщо шукати неіснуючу книгу, то повернеться underfind; база данних, якщо не знаходить дані, повертає null
+  return result || null;
+};
 
 const addContact = async (name, email, phone) => {
-const contacts = await listContacts();
-const newContact = {
-    name, 
+  // отримуємо всі контакти
+  const contacts = await listContacts();
+  // створюємо новий контакт
+  const newContact = {
+    name,
     email,
     phone,
-    id: nanoid(), 
-}
-contacts.push(newContact)
-await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-return newContact;
-}
+    id: nanoid(),
+  };
+  // в масив контактів додаємо новий контакт
+  contacts.push(newContact);
+  // повністю перезаписуємо json
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  // повертаємо новий контакт
+  return newContact;
+};
+
+const removeContact = async (id) => {
+  const contactId = String(id);
+  // отримуємо всі контакти
+  const contacts = await listContacts();
+  // шукаємо індекс контакту, який треба видалити
+  const index = contacts.findIndex((el) => el.id === contactId);
+  // якщо findIndex не знаходить, то він повертає -1
+  if (index === -1) {
+    return null;
+  }
+  // splice вирізає контакт по індексу
+  const [result] = contacts.splice(index, 1);
+  // повністю перезаписуємо json
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
+
+const updateContact = async (id, data) => {
+  // отримуємо всі контакти
+  const contacts = await listContacts();
+  // шукаємо індекс контакту, який треба оновити
+  const index = contacts.findIndex((el) => el.id === id);
+  // якщо findIndex не знаходить, то він повертає -1
+  if (index === -1) {
+    return null;
+  }
+  // якщо є індекс, повністю перезаписуэмо контакти
+  contacts[index] = { id, ...data };
+  // повністю перезаписуємо json
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+};
 
 module.exports = {
-    listContacts,
-    getContactById,
-    removeContact,
-    addContact,
-}
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
